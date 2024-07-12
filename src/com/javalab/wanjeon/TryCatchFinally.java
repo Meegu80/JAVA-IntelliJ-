c233473dc2a241beda9bfb0160cc87a7e3add9ae
Meegu80 <aceman80@nate.com>
2024-07-13 00:15:06 +0900

    예외처리

---
+++ src/com/javalab/wanjeon/TryCatchFinally.java
@@ -0,0 +1,29 @@
+package com.javalab.wanjeon;
+
+public class TryCatchFinally {
+    public static void main(String[] args) {
+
+    // 1. try-catch
+    try {
+        System.out.println(3 / 0); // 실행 예외
+        System.out.println("프로그램 종료");
+    }
+    catch (ArithmeticException e){
+        System.out.println("숫자는 0으로 나눌 수 없습니다.");
+        System.out.println("프로그램 종료");
+    }
+
+    // 2. try-catch-finally
+    try {
+        System.out.println(3 / 0); // 실행 예외
+    }
+    catch (ArithmeticException e)
+
+    {
+        System.out.println("숫자는 0으로 나눌 수 없습니다.");
+    }
+    finally {
+        System.out.println("프로그램 종료");
+    }
+    }
+}
